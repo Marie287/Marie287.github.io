a1e19ff82996b65f20286ce8f1287e9de05c5a69
Marie287 <marie.bruggeman287@gmail.com>
2021-03-16 21:18:51 +0100

    Suite

--- js/index.js
+++ js/index.js
@@ -2,19 +2,16 @@ $( document ).ready(function() {
   $('#m_wsc').load('./m.wsc.html');
   $('#m_ose').load('./m.ose.html');
   $('#m_bomb').load('./m.bomb.html');
+  $('#m_ctq').load('./m.ctq.html');
+  $('#m_mps').load('./m.mps.html');
+  $('#m_pm').load('./m.pm.html');
+  $('#m_fs').load('./m.fs.html');
+  $('#m_sm').load('./m.sm.html');
 
   // Projets - size of cards
   if( $("#card_films").height() > $("#card_med").height()) $(".card-body").height($("#card_films > .card-body").height());
   else $(".card-body").height($("#card_med > .card-body").height());
 
-  /*
-  var myModalEl = document.getElementById('m_ose')
-  myModalEl.addEventListener('show.bs.modal', function (event) {
-    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-      return new bootstrap.Tooltip(tooltipTriggerEl)
-    })
-  })*/
 });
 
 
