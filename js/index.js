998f76daf36e83555717916b5dbdd12a167ab8bd
Marie287 <marie.bruggeman287@gmail.com>
2021-03-17 23:18:41 +0100

    Parcours

--- js/index.js
+++ js/index.js
@@ -12,6 +12,23 @@ $( document ).ready(function() {
   if( $("#card_films").height() > $("#card_med").height()) $(".card-body").height($("#card_films > .card-body").height());
   else $(".card-body").height($("#card_med > .card-body").height());
 
+
+  // Rotation flèche collapse (Détails dans parcours.html)
+  $(".btn_parcours").on("click", function(){
+    if($(this).find(".arrow").hasClass("down")){
+      $(this).find(".arrow").removeClass("down");
+      $(this).find(".arrow").addClass("up");
+      $(this).parent().addClass("p_open");
+      $(this).addClass("btn_parcours_open");
+    }
+    else{
+      $(this).find(".arrow").removeClass("up");
+      $(this).find(".arrow").addClass("down");
+      $(this).parent().removeClass("p_open");
+      $(this).removeClass("btn_parcours_open");
+    }
+  });
+
 });
 
 
